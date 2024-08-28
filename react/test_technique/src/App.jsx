5a37c506db154984a6b6b401eea288eb59c8b811
VRuddy <ruddy.villain@epitech.eu>
2024-08-28 15:09:50 +0200

    Add description

--- react/test_technique/src/App.jsx
+++ react/test_technique/src/App.jsx
@@ -1,18 +1,24 @@
-import './App.css'
-import Counter from './components/Counter'
-import Greeting from './components/Greeting'
-import NamesList from './components/NamesList'
+import "./App.css";
+import Counter from "./components/Counter";
+import Greeting from "./components/Greeting";
+import NamesList from "./components/NamesList";
 
 function App() {
-  const names = ['Alice', 'Bob', 'Charlie', 'David'];
+  const names = ["Alice", "Bob", "Charlie", "David"];
   return (
     <>
       <h1>Test technique</h1>
-      <Greeting name={"Check"}/>
-      <Counter/>
-      <NamesList names={names}/>
+
+      <h2>Greeting component</h2>
+      <Greeting name={"Check"} />
+
+      <h2>Counter component</h2>
+      <Counter />
+
+      <h2>NamesList component</h2>
+      <NamesList names={names} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
